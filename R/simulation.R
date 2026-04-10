9650646979a4bfb832f45ea60a031c262fef8bc4
MIKA19325 <mikaschubi@gmail.com>
2026-04-10 14:49:33 +0200

    Simulation with one new class for simulation output and own plot function

--- R/simulation.R
+++ R/simulation.R
@@ -26,21 +26,20 @@ simulateLayer <- function(size = 200,
   resolution <- 1/resolution
   r <- terra::rast(ncols = size / resolution,
                    nrows = size / resolution,
-                   ext = terra::ext(-0.5 * size, 0.5 * size, -0.5 * size, 0.5 * size))  #create raster with defined habitat size and resolution
-  terra::values(r) <- stats::rnorm(terra::ncell(r))  #normal distribution on the raster
-
+                   ext = terra::ext(-0.5 * size, 0.5 * size, -0.5 * size, 0.5 * size))
+  terra::values(r) <- stats::rnorm(terra::ncell(r))
 
   r_smooth <- terra::focal(r, w = terra::focalMat(r, clustering, "Gauss"),
                            na.policy = "omit",
-                           expand = TRUE) #smoothing according to neighboring weights
+                           expand = TRUE)
 
-  threshold <- stats::quantile(terra::values(r_smooth), 1-ratio, na.rm=TRUE)  #binarization
+  threshold <- stats::quantile(terra::values(r_smooth), 1-ratio, na.rm=TRUE)
   hab <- r_smooth >= threshold
 
-  layer <- tibble::tibble(raster = list(hab), layerColour = colour)
-  class(layer) = "citoMoveLayer"
+  names(hab) <- colour
+  attr(hab, "layerColour") <- colour
 
-  return(layer)
+  return(hab)
 }
 
 
@@ -60,7 +59,7 @@ simulateLayer <- function(size = 200,
 #' @param kappaTA the kappa parameter of the von mises distribution of the turning angle
 #' @param colour colour of the track for plotting
 #' @param numericLayers a list of the numeric raster layers
-#' @param rasterLayers a list of the raster layers of the habitat vraiables. Must be in form rasterLayers = list(...)
+#' @param rasterLayers a list of the raster layers of the habitat variables. Must be in form rasterLayers = list(...)
 #' @param betas a vector of the preference values of the habitat variables. Must be same length as the list of numericLayers
 #'
 #' @details
@@ -83,73 +82,70 @@ simulateTrack <- function(xStart = 0,
                           colour = "black",
                           numericLayers = NULL,
                           imageLayers = NULL,
-                          numericlayers = NULL,
-                          imageLayers = NULL,
                           betas = c(-3)){
 
-  if(is.null(numericLayers)) {numericLayers <- list(simulateLayer(plot = FALSE))}
+  if(is.null(numericLayers)) {numericLayers <- list(simulateLayer())}
 
-  rasters <- lapply(numericLayers, function(x) x$raster[[1]])
-  layerColours <- sapply(numericLayers, function(x) x$layerColour)
+  rasters <- numericLayers
 
+  layerColours <- sapply(numericLayers, function(x) attr(x, "layerColour"))
 
+  simData <- tibble::tibble(stepID    = 0:nSteps,
+                            x_        = numeric(nSteps + 1),
+                            y_        = numeric(nSteps + 1),
+                            SL        = numeric(nSteps + 1),
+                            Direction = numeric(nSteps + 1),
+                            Change    = numeric(nSteps + 1),
+                            habitat   = numeric(nSteps + 1))
 
-
-  simData <- tibble::tibble(stepID = 0:nSteps,
-                        x = numeric(nSteps + 1),
-                        y = numeric(nSteps + 1),
-                        SL = numeric(nSteps + 1),
-                        Direction = numeric(nSteps + 1),
-                        Change = numeric(nSteps + 1),
-                        habitat = numeric(nSteps + 1))  #allocating data frame
-
-
-
-  simData$x[1] <- xStart
-  simData$y[1] <- yStart
+  simData$x_[1] <- xStart
+  simData$y_[1] <- yStart
   simData$SL[1] <- 0
   simData$Direction[1] <- angleStart
   simData$Change[1] <- 0
-  simData$habitat[1] <- sum(sapply(seq_along(rasters), function(k) betas[[k]] * terra::extract(rasters[[k]], cbind(simData$x[1], simData$y[1]))[,1])) #simulating starting points
-
-
-
+  simData$habitat[1] <- sum(sapply(seq_along(rasters), function(k)
+    betas[[k]] * terra::extract(rasters[[k]], cbind(simData$x_[1], simData$y_[1]))[,1]))
 
   for(i in 1:nSteps) {
 
-    SLrel <- stats::rgamma(nChoiceSet, shape = shapeSL, scale = scaleSL)
+    SLrel     <- stats::rgamma(nChoiceSet, shape = shapeSL, scale = scaleSL)
     ChangeRad <- circular::rvonmises(nChoiceSet, mu = circular::circular(0), kappa = kappaTA)
-    Change <- as.numeric(ChangeRad) * 180/pi
-    TArel <- simData$Direction[i] + Change
-    x <- simData$x[i] + SLrel * sin(TArel * pi/180)
-    y <- simData$y[i] + SLrel * cos(TArel * pi/180)
-    border <- terra::ext(rasters[[1]])
-    x <- ((x - border[1]) %% (border[2] - border[1])) + border[1]
-    y <- ((y - border[3]) %% (border[4] - border[3])) + border[3]
-    Direction <- (atan2(x-simData$x[i], y-simData$y[i]) * 180/pi) %%360
+    Change    <- as.numeric(ChangeRad) * 180/pi
+    TArel     <- simData$Direction[i] + Change
+    x         <- simData$x_[i] + SLrel * sin(TArel * pi/180)
+    y         <- simData$y_[i] + SLrel * cos(TArel * pi/180)
+    border    <- terra::ext(rasters[[1]])
+    x         <- ((x - border[1]) %% (border[2] - border[1])) + border[1]
+    y         <- ((y - border[3]) %% (border[4] - border[3])) + border[3]
+    Direction <- (atan2(x - simData$x_[i], y - simData$y_[i]) * 180/pi) %% 360
 
-    linpred <- Reduce("+", lapply(seq_along(rasters), function(k) betas[[k]] * terra::extract(rasters[[k]], cbind(x, y))[,1]))
-
+    linpred <- Reduce("+", lapply(seq_along(rasters), function(k)
+      betas[[k]] * terra::extract(rasters[[k]], cbind(x, y))[,1]))
     linpred <- linpred - max(linpred)
-    p <- exp(linpred) / sum(exp(linpred))
+    p       <- exp(linpred) / sum(exp(linpred))
 
-    multinom <- stats::rmultinom(1, size = 1, prob = p)
+    multinom   <- stats::rmultinom(1, size = 1, prob = p)
     chosenStep <- which(multinom == 1)
 
-
-    simData$x[i+1] <- x[chosenStep]
-    simData$y[i+1] <- y[chosenStep]
-    simData$SL[i+1] <- SLrel[chosenStep]
+    simData$x_[i+1]        <- x[chosenStep]
+    simData$y_[i+1]        <- y[chosenStep]
+    simData$SL[i+1]        <- SLrel[chosenStep]
     simData$Direction[i+1] <- Direction[chosenStep]
-    simData$Change[i+1] <- Change[chosenStep]
-    simData$habitat[i+1] <- sum(sapply(seq_along(rasters), function(k) betas[[k]] * terra::extract(rasters[[k]], cbind(x[chosenStep], y[chosenStep]))[,1]))
+    simData$Change[i+1]    <- Change[chosenStep]
+    simData$habitat[i+1]   <- sum(sapply(seq_along(rasters), function(k)
+      betas[[k]] * terra::extract(rasters[[k]], cbind(x[chosenStep], y[chosenStep]))[,1]))
 
-    } #simulating tracks
+  }
 
-    simData$time <- seq(from = tStart, by = "hour", length.out = nrow(simData))
+  simData$t_ <- seq(from = tStart, by = "hour", length.out = nrow(simData))
 
-  track <- tibble::tibble(track = list(simData), trackColour = colour, layers = list(numericLayers))
-  class(track) = "citoMoveTrack"
+  track <- amt::make_track(simData, x_, y_, t_,
+                           SL = SL, Direction = Direction,
+                           Change = Change, habitat = habitat,
+                           stepID = stepID)
+
+  attr(track, "trackColour") <- colour
+  attr(track, "layers")      <- numericLayers
 
   return(track)
 }
@@ -165,15 +161,16 @@ simulateTrack <- function(xStart = 0,
 
 mergeTracks <- function(...){
 
-  tracks = list(...)
+  tracks <- list(...)
 
   trackData <- do.call(rbind, lapply(seq_along(tracks), function(i){
-    df <- tracks[[i]]$track[[1]]
-    df$trackID <- i
-    df$trackColour <- tracks[[i]]$trackColour
-    df}))
+    df             <- as.data.frame(tracks[[i]])
+    df$trackID     <- i
+    df$trackColour <- attr(tracks[[i]], "trackColour")
+    df
+  }))
 
-  layers <- tracks[[1]]$layers[[1]]
+  layers <- attr(tracks[[1]], "layers")
 
   simulation <- tibble::tibble(tracks = list(trackData), layer = list(layers))
   class(simulation) <- "citoMoveSimulation"
@@ -185,73 +182,35 @@ mergeTracks <- function(...){
 
 #' @title Plot Simulated Objects
 #'
-#' @description Plots one or more objects of class 'Layer', one or more objects of class 'Track', or a single object of class 'Simulation'.The habitat variable layers of the tracks have to be identical
+#' @description Plots a single object of class 'citoMoveSimulation'.
+#'
+#' @param x object of class citoMoveSimulation
 #'
 #' @export
 #' @author Mika Schubert
-#'
-#'
 
-#plot.citoMoveTrack(x, ...)
+plot.citoMoveSimulation <- function(x) {
 
-plot(x)
+  layers    <- x$layer[[1]]
+  trackData <- x$tracks[[1]]
+  trackIDs  <- unique(trackData$trackID)
+  layerColours <- sapply(layers, function(l) attr(l, "layerColour"))
 
+  terra::plot(layers[[1]], col = c(NA, layerColours[1]),
+              legend = FALSE, alpha = 1/length(layers))
+  if(length(layers) > 1) {
+    for(l in 2:length(layers)){
+      terra::plot(layers[[l]], col = c(NA, layerColours[l]),
+                  legend = FALSE, alpha = 1/length(layers), add = TRUE)
+    }
+  }
 
-plotSimulation <- function(...) {
-
-  objects <- list(...)
-
-  plotLayers <- function(layers) {
-    rasters <- lapply(layers, function(x) x$raster[[1]])
-    layerColours <- sapply(layers, function(x) x$layerColour)
-
-    terra::plot(rasters[[1]], col = c(NA, layerColours[1]),
-                legend = FALSE, alpha = 1/length(rasters))
-    if(length(rasters) > 1) {
-      for(l in 2:length(rasters)){
-        terra::plot(rasters[[l]], col = c(NA, layerColours[l]),
-                    legend = FALSE, alpha = 1/length(rasters), add = TRUE)}}
-  } # helper function to plot single or multiple layers
-
-
-  plotTrack <- function(trackData, trackColour) {
-    lines(trackData$x, trackData$y, col = trackColour)
-    points(trackData$x[1], trackData$y[1], pch = 16, col = trackColour)
-  } # helper function to plot single track
-
-
-
-  if(all(sapply(objects, function(o) inherits(o, "citoMoveLayer")))) {
-    plotLayers(objects)
-  } # case 1: one or multiple layers
-
-
-  else if(all(sapply(objects, function(o) inherits(o, "citoMoveTrack")))) {
-    layers <- objects[[1]]$layers[[1]]
-    plotLayers(layers)
-
-    for(i in seq_along(objects)){
-      trackData <- objects[[i]]$track[[1]]
-      trackColour <- objects[[i]]$trackColour
-      plotTrack(trackData, trackColour)}
-  } # case 2: one or multiple tracks
-
-
-  else if(length(objects) == 1 && inherits(objects[[1]], "citoMoveSimulation")) {
-    simulation <- objects[[1]]
-    layers <- simulation$layer[[1]]
-    plotLayers(layers)
-
-    trackData <- simulation$tracks[[1]]
-    trackIDs <- unique(trackData$trackID)
-
-    for(m in trackIDs){
-      dat <- trackData[trackData$trackID == m, ]
-      trackColour <- dat$trackColour[1]  # Farbe aus den Daten
-      plotTrack(dat, trackColour)}
-  } # case 3: a single simulation
-
-  else {
-    stop("Input must be one or more objects of class 'Layer', one or more objects of class 'Track', or a single object of class 'Simulation'")
+  for(m in trackIDs){
+    dat         <- trackData[trackData$trackID == m, ]
+    trackColour <- dat$trackColour[1]
+    lines(dat$x_, dat$y_, col = trackColour)
+    points(dat$x_[1], dat$y_[1], pch = 16, col = trackColour)
   }
 }
+
+
--- inst/examples/simulation-example.R
+++ inst/examples/simulation-example.R
@@ -1,44 +1,27 @@
+library(terra)
+library(amt)
 
+# simulate layers
+layer1 <- simulateLayer(size = 200, clustering = 15, ratio = 0.5, colour = "darkgreen")
+layer2 <- simulateLayer(size = 200, clustering = 20, ratio = 0.3, colour = "steelblue")
 
-#Example: Simulating 2 animal tracks in an environment with 3 habitat variable layers
-
-layer1 <- simulateLayer(size = 200,
-                        resolution = 1,
-                        clustering  = 15,
-                        ratio = 0.3,
-                        colour = "darkgreen")
-
-plotSimulation(layer1)
-
-# für alles schon mal auch testthat funktionen schreiben
-
-track2 <- simulateTrack(xStart = 0,
+# simulate tracks
+track1 <- simulateTrack(xStart = 0,
                         yStart = 0,
-                        tStart = as.POSIXct("2026-01-01 00:00:00",tz = "UTC"),
-                        angleStart = 0,
                         nSteps = 100,
-                        nChoiceSet = 10,
-                        scaleSL = 2,
-                        shapeSL = 2,
-                        kappaTA = 2,
-                        numericLayers = list(layer1, layer2, layer3),
-                        betas = c(-5, 2, 3),
-                        colours = c("darkgreen", "darkblue", "darkred"))
+                        colour = "black",
+                        numericLayers = list(layer1, layer2),
+                        betas = c(-5, -3))
 
+track2 <- simulateTrack(xStart = 10,
+                        yStart = 10,
+                        nSteps = 100,
+                        colour = "red",
+                        numericLayers = list(layer1, layer2),
+                        betas = c(-5, 3))
 
-a <- simulateLayer(colour = "darkgreen")
-plotSimulation(a)
+# merge tracks
+simulation <- mergeTracks(track1, track2)
 
-b <- simulateLayer(colour = "darkblue")
-plotSimulation(b)
-plotSimulation(a, b)
-
-t1 <- simulateTrack(numericLayers = list(a,b), betas = c(-3,3), colour = "black")
-plotSimulation(t1)
-
-t2 <- simulateTrack(numericLayers = list(a,b), betas = c(0,3), colour = "darkred")
-plotSimulation(t2)
-plotSimulation(t1, t2)
-sim <- mergeTracks(t1, t2)
-
-plotSimulation(sim)
+# plot
+plot(simulation)
